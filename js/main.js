a79377691d26a3beef86944760c971a8a1035d1b
Robofish996 <Matthew.A.Riddle@outlook.com>
2023-04-15 02:54:30 +0200

    cleaning code
    editing css and main js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 //Trigger the slide up to show the main content
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
     let button = document.querySelector(".btn");
     let cover = document.querySelector(".cover");
-    button.addEventListener("click", function() {
-      cover.classList.add("slideUp");
+    button.addEventListener("click", function () {
+        cover.classList.add("slideUp");
     });
-  });
-  
+});
