bd311fd40b7a5dc1725c07adcf9631fc7a83b779
Robofish996 <Matthew.A.Riddle@outlook.com>
2023-04-17 00:59:59 +0200

    Using Js to add products and styled buttons&images

--- js/main.js
+++ js/main.js
@@ -3,12 +3,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let button = document.querySelector(".btn");
   let cover = document.querySelector(".cover");
   button.addEventListener("click", function () {
-      cover.classList.add("slideUp");
+    cover.classList.add("slideUp");
   });
 });
-
-// Select the element where the product items will be appended
-const productList = document.querySelector(".product-list");
 
 // Fetch the product data from the JSON file
 fetch('./js/data.json')
@@ -16,35 +13,59 @@ fetch('./js/data.json')
   .then(response => response.json())
   // Once the data is retrieved, loop through each product
   .then(products => {
-    products.forEach(product => {
-      // Create a div element to hold each product
-      const productItem = document.createElement("div");
-      // Add the "product-item" and "card" classes to the product div
-      productItem.classList.add("product-item", "card");
+    // Categorize the products based on their IDs
+    const loungeProducts = products.filter(product => product.id >= 1 && product.id <= 10);
+    const diningProducts = products.filter(product => product.id >= 11 && product.id <= 20);
+    const studyProducts = products.filter(product => product.id >= 21 && product.id <= 30);
 
-      // Categorize the product based on its ID
-      let category = "";
-      if (product.id >= 1 && product.id <= 10) {
-        category = "Lounge Suite";
-      } else if (product.id >= 11 && product.id <= 20) {
-        category = "Dining Room Suite";
-      } else if (product.id >= 21 && product.id <= 30) {
-        category = "Study";
+    console.log("loungeProducts:", loungeProducts);
+    console.log("diningProducts:", diningProducts);
+    console.log("studyProducts:", studyProducts);
+
+    // Select all the card elements
+    const cards = document.querySelectorAll('.card');
+
+    // Loop through each card element and populate it with product data
+    for (let i = 0; i < cards.length; i++) {
+      const card = cards[i];
+
+      // Find the corresponding product for this card
+      let product;
+      if (i < loungeProducts.length) {
+        product = loungeProducts[i];
+      } else if (i < loungeProducts.length + diningProducts.length) {
+        product = diningProducts[i - loungeProducts.length];
+      } else {
+        product = studyProducts[i - loungeProducts.length - diningProducts.length];
       }
 
-      // Set the HTML content of the product div with the product data
-      productItem.innerHTML = `
-        <img src="${product.image}" alt="${product.name}" class="card-img-top">
-        <div class="card-body">
-          <h3 class="card-title">${product.name}</h3>
-          <p class="card-text">${product.description}</p>
-          <span class="card-price">R${product.price}</span>
-          <span class="card-category">${category}</span>
-          <button class="btn btn-primary">Add to Cart</button>
-        </div>
-      `;
+      // Set the card image
+      const img = document.createElement('img');
+      img.src = product.image;
+      card.appendChild(img);
 
-      // Append the product div to the product list
-      productList.appendChild(productItem);
-    });
+      // Set the card title
+      const title = document.createElement('h2');
+      title.textContent = product.name;
+      card.appendChild(title);
+
+      // Set the card description
+      const desc = document.createElement('p');
+      desc.textContent = product.description;
+      card.appendChild(desc);
+
+      // Set the card price
+      const price = document.createElement('p');
+      price.textContent = `R${product.price.toFixed(2)}`;
+      card.appendChild(price);
+
+      // Add an "Add to Cart" button
+      const button = document.createElement('button');
+      button.textContent = 'Add to Cart';
+      button.addEventListener('click', function() {
+        // Here you can add code to add the product to the cart
+        console.log(`Added ${product.name} to cart`);
+      });
+      card.appendChild(button);
+    }
   });
